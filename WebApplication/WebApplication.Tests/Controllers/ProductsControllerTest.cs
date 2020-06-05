7cd690689c056dbc6685fb15508c1de6c352ebbf
Sakura Ayame Kinomoto <55733660+phamquocduy1110@users.noreply.github.com>
2020-06-05 11:58:02 +0700

    Add eCommerce web application

---
+++ WebApplication/WebApplication.Tests/Controllers/ProductsControllerTest.cs
@@ -0,0 +1,262 @@
+﻿using Moq;
+using System;
+using System.Web;
+using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication.Controllers;
+using WebApplication.Models;
+using System.Transactions;
+
+namespace WebApplication.Tests.Controllers
+{
+    [TestClass]
+    public class ProductsControllerTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var controller = new ProductsController();
+
+            var result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+
+            var model = result.Model as List<Product>;
+            Assert.IsNotNull(model);
+
+            var db = new CS4PEntities();
+            Assert.AreEqual(db.Products.Count(), model.Count);
+        }
+
+        [TestMethod]
+        public void TestIndex2()
+        {
+            var controller = new ProductsController();
+
+            var result = controller.Index2() as ViewResult;
+            Assert.IsNotNull(result);
+
+            var model = result.Model as List<Product>;
+            Assert.IsNotNull(model);
+
+            var db = new CS4PEntities();
+            Assert.AreEqual(db.Products.Count(), model.Count);
+        }
+
+        [TestMethod]
+        public void TestCreateG()
+        {
+            var controller = new ProductsController();
+
+            var result = controller.Create() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void TestCreateP()
+        {
+            var rand = new Random();
+            var product = new Product
+            {
+                Name = rand.NextDouble().ToString(),
+                Description = rand.NextDouble().ToString(),
+                Price = -rand.Next()
+            };
+
+            var controller = new ProductsController();
+
+            var result0 = controller.Create(product, null) as ViewResult;
+            Assert.IsNotNull(result0);
+            Assert.AreEqual("Price is less than Zero", controller.ModelState["Price"].Errors[0].ErrorMessage);
+
+            product.Price = -product.Price;
+            controller.ModelState.Clear();
+
+            result0 = controller.Create(product, null) as ViewResult;
+            Assert.IsNotNull(result0);
+            Assert.AreEqual("Picture not found!", controller.ModelState[""].Errors[0].ErrorMessage);
+
+            var picture = new Mock<HttpPostedFileBase>();
+            var server = new Mock<HttpServerUtilityBase>();
+            var context = new Mock<HttpContextBase>();
+            context.Setup(c => c.Server).Returns(server.Object);
+            controller.ControllerContext = new ControllerContext(context.Object,
+                new System.Web.Routing.RouteData(), controller);
+
+            var fileName = String.Empty;
+            server.Setup(s => s.MapPath(It.IsAny<string>())).Returns<string>(s => s);
+            picture.Setup(p => p.SaveAs(It.IsAny<string>())).Callback<string>(s => fileName = s);
+
+            using (var scope = new TransactionScope())
+            {
+                controller.ModelState.Clear();
+                var result1 = controller.Create(product, picture.Object) as RedirectToRouteResult;
+                Assert.IsNotNull(result1);
+                Assert.AreEqual("Index", result1.RouteValues["action"]);
+
+                var db = new CS4PEntities();
+                var entity = db.Products.SingleOrDefault(p => p.Name == product.Name && p.Description == product.Description);
+                Assert.IsNotNull(entity);
+                Assert.AreEqual(product.Price, entity.Price);
+
+                Assert.IsTrue(fileName.StartsWith("~/Upload/Products/"));
+                Assert.IsTrue(fileName.EndsWith(entity.id.ToString()));
+            }
+        }
+
+        [TestMethod]
+        public void TestEditG()
+        {
+            var controller = new ProductsController();
+            var result0 = controller.Edit(0) as HttpNotFoundResult;
+            Assert.IsNotNull(result0);
+
+            var db = new CS4PEntities();
+            var product = db.Products.First();
+            var result1 = controller.Edit(product.id) as ViewResult;
+            Assert.IsNotNull(result1);
+
+            var model = result1.Model as Product;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(product.Name, model.Name);
+            Assert.AreEqual(product.Price, model.Price);
+            Assert.AreEqual(product.Description, model.Description);
+        }
+
+        [TestMethod]
+        public void TestEditP()
+        {
+            var rand = new Random();
+            var db = new CS4PEntities();
+            var product = db.Products.AsNoTracking().First();
+            product.Name = rand.NextDouble().ToString();
+            product.Description = rand.NextDouble().ToString();
+            product.Price = -rand.Next();
+
+            var controller = new ProductsController();
+
+            var result0 = controller.Edit(product, null) as ViewResult;
+            Assert.IsNotNull(result0);
+            Assert.AreEqual("Price is less than Zero", controller.ModelState["Price"].Errors[0].ErrorMessage);
+
+            var picture = new Mock<HttpPostedFileBase>();
+            var server = new Mock<HttpServerUtilityBase>();
+            var context = new Mock<HttpContextBase>();
+            context.Setup(c => c.Server).Returns(server.Object);
+            controller.ControllerContext = new ControllerContext(context.Object,
+                new System.Web.Routing.RouteData(), controller);
+
+            var fileName = String.Empty;
+            server.Setup(s => s.MapPath(It.IsAny<string>())).Returns<string>(s => s);
+            picture.Setup(p => p.SaveAs(It.IsAny<string>())).Callback<string>(s => fileName = s);
+
+            using (var scope = new TransactionScope())
+            {
+                product.Price = -product.Price;
+                controller.ModelState.Clear();
+                var result1 = controller.Edit(product, picture.Object) as RedirectToRouteResult;
+                Assert.IsNotNull(result1);
+                Assert.AreEqual("Index", result1.RouteValues["action"]);
+
+                var entity = db.Products.Find(product.id);
+                Assert.IsNotNull(entity);
+                Assert.AreEqual(product.Name, entity.Name);
+                Assert.AreEqual(product.Description, entity.Description);
+                Assert.AreEqual(product.Price, entity.Price);
+
+                Assert.AreEqual("~/Upload/Products/" + product.id, fileName);
+                //Assert.IsTrue(fileName.StartsWith("~/Upload/Products/"));
+                //Assert.IsTrue(fileName.EndsWith(entity.id.ToString()));
+            }
+        }
+
+        [TestMethod]
+        public void TestDeleteG()
+        {
+            var controller = new ProductsController();
+            var result0 = controller.Delete(0) as HttpNotFoundResult;
+            Assert.IsNotNull(result0);
+
+            var db = new CS4PEntities();
+            var product = db.Products.First();
+            var result1 = controller.Delete(product.id) as ViewResult;
+            Assert.IsNotNull(result1);
+
+            var model = result1.Model as Product;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(product.Name, model.Name);
+            Assert.AreEqual(product.Price, model.Price);
+            Assert.AreEqual(product.Description, model.Description);
+        }
+
+        [TestMethod]
+        public void TestDeleteP()
+        {
+            var db = new CS4PEntities();
+            var product = db.Products.AsNoTracking().First();
+
+            var controller = new ProductsController();
+
+            var context = new Mock<HttpContextBase>();
+            var server = new Mock<HttpServerUtilityBase>();
+            context.Setup(c => c.Server).Returns(server.Object);
+            controller.ControllerContext = new ControllerContext(context.Object,
+                new System.Web.Routing.RouteData(), controller);
+
+            var filePath = String.Empty;
+            var tempDir = System.IO.Path.GetTempFileName();
+            server.Setup(s => s.MapPath(It.IsAny<string>())).Returns<string>(s =>
+            {
+                filePath = s;
+                return tempDir;
+            });
+
+            using (var scope = new TransactionScope())
+            {
+                System.IO.File.Delete(tempDir);
+                System.IO.Directory.CreateDirectory(tempDir);
+                tempDir = tempDir + "/";
+                System.IO.File.Create(tempDir + product.id).Close();
+                Assert.IsTrue(System.IO.File.Exists(tempDir + product.id));
+
+                var result = controller.DeleteConfirmed(product.id) as RedirectToRouteResult;
+                Assert.IsNotNull(result);
+                Assert.AreEqual("Index", result.RouteValues["action"]);
+
+                var entity = db.Products.Find(product.id);
+                Assert.IsNull(entity);
+
+                Assert.AreEqual("~/Upload/Products/", filePath);
+                Assert.IsFalse(System.IO.File.Exists(tempDir + product.id));
+            }
+        }
+
+        [TestMethod]
+        public void TestPicture()
+        {
+            var controller = new ProductsController();
+
+            var context = new Mock<HttpContextBase>();
+            var server = new Mock<HttpServerUtilityBase>();
+            context.Setup(c => c.Server).Returns(server.Object);
+            controller.ControllerContext = new ControllerContext(context.Object,
+                new System.Web.Routing.RouteData(), controller);
+
+            var filePath = String.Empty;
+            server.Setup(s => s.MapPath(It.IsAny<string>())).Returns<string>(s => filePath = s);
+
+            var result = controller.Picture(0) as FilePathResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual("~/Upload/Products/0", result.FileName);
+            Assert.AreEqual("images", result.ContentType);
+        }
+
+        [TestMethod]
+        public void TestDispose()
+        {
+            using (var controller = new ProductsController()) { }
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Controllers/ProductsController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Transactions;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class ProductsController : Controller
+    {
+        private CS4PEntities db = new CS4PEntities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var model = db.Products.ToList();
+            return View(model);
+        }
+
+        // for customer to view products
+        [AllowAnonymous]
+        public ActionResult Index2()
+        {
+            var model = db.Products.ToList();
+            return View(model);
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        [AllowAnonymous]
+        public ActionResult Picture(int id)
+        {
+            var path = Server.MapPath(PICTURE_PATH);
+            return File(path + id, "images");
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost, ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product model, HttpPostedFileBase picture)
+        {
+            ValidateProduct(model);
+            if (ModelState.IsValid)
+            {
+                if (picture != null)
+                {
+                    using (var scope = new TransactionScope())
+                    {
+                        db.Products.Add(model);
+                        db.SaveChanges();
+
+                        // store picture
+                        var path = Server.MapPath(PICTURE_PATH);
+                        picture.SaveAs(path + model.id);
+
+                        scope.Complete();
+                        return RedirectToAction("Index");
+                    }
+                }
+                else ModelState.AddModelError("", "Picture not found!");
+            }
+
+            return View(model);
+        }
+
+        private const string PICTURE_PATH = "~/Upload/Products/";
+
+        private void ValidateProduct(Product product)
+        {
+            if (product.Price < 0)
+                ModelState.AddModelError("Price", "Price is less than Zero");
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var model = db.Products.Find(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost, ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product model, HttpPostedFileBase picture)
+        {
+            ValidateProduct(model);
+            if (ModelState.IsValid)
+            {
+                using (var scope = new TransactionScope())
+                {
+                    db.Entry(model).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                    if (picture != null)
+                    {
+                        var path = Server.MapPath(PICTURE_PATH);
+                        picture.SaveAs(path + model.id);
+                    }
+
+                    scope.Complete();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(model);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var model = db.Products.Find(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            using (var scope = new TransactionScope())
+            {
+                var model = db.Products.Find(id);
+                db.Products.Remove(model);
+                db.SaveChanges();
+
+                var path = Server.MapPath(PICTURE_PATH);
+                System.IO.File.Delete(path + model.id);
+
+                scope.Complete();
+                return RedirectToAction("Index");
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
